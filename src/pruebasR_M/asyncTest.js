5a5950fc56b35eb7b553f278ed0a09337fcc4ba7
Mateo Monsalve <mateomch98@gmail.com>
2021-03-10 15:56:26 -0500

    test primero

--- src/pruebasR_M/asyncTest.js
+++ src/pruebasR_M/asyncTest.js
@@ -1,19 +1,20 @@
 const fetchData = require('../utils/fetchData')
-const API = 'https://rickandmortyapi.com/api/character/'
+const API = 'https://swapi.dev/api/'
+const PEOPLE_URL = 'people/:id'
 
-const theFunction = async(url_api) => {
+const theFunction = async(url_api, id) => {
     try {
         const data = await fetchData(url_api)
-        const character = await fetchData(`${API}${data.results[0].id}`)
-        const origin = await fetchData(character.origin.url)
+        const character = await fetchData(`${API}${PEOPLE_URL.replace(':id', id)}`)
+        const people = await fetchData(people.origin.url)
         console.log(data.info.count)
         console.log(character.name)
-        console.log(origin.dimension)
+        console.log(origin.gender)
     } catch(error) {
         console.error(error)
     }
 }
 
 console.log('before')
-theFunction(API)
+theFunction(API, 1)
 console.log('after')
