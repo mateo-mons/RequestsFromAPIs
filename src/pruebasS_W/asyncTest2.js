503fd5229a46bcb54b5c6f936f39eac43862b6d5
Mateo Monsalve <mateomch98@gmail.com>
2021-03-11 17:33:37 -0500

    nuevo async de SW

---
+++ src/pruebasS_W/asyncTest2.js
@@ -0,0 +1,15 @@
+const fetchData = require('../utils/fetchData')
+const API = 'https://swapi.dev/api/'
+const PEOPLE_URL = 'people/:id'
+
+const theFunction = async(id) => {
+    try {
+        const people = await fetchData(`${API}${PEOPLE_URL.replace(':id', id)}`)
+        console.log(people.name)
+        console.log(people.gender)
+    } catch(error) {
+        console.error(error)
+    }
+}
+
+theFunction(1)
