903fe3a61a3c561bbdd6e223728d058b1ae69619
Mateo Monsalve <mateomch98@gmail.com>
2021-03-11 16:41:18 -0500

    cambio de APIs de R&M y SW

--- src/pruebasR_M/asyncTest.js
+++ src/pruebasR_M/asyncTest.js
@@ -2,19 +2,14 @@ const fetchData = require('../utils/fetchData')
 const API = 'https://swapi.dev/api/'
 const PEOPLE_URL = 'people/:id'
 
-const theFunction = async(url_api, id) => {
+const theFunction = async(id) => {
     try {
-        const data = await fetchData(url_api)
-        const character = await fetchData(`${API}${PEOPLE_URL.replace(':id', id)}`)
-        const people = await fetchData(people.origin.url)
-        console.log(data.info.count)
-        console.log(character.name)
-        console.log(origin.gender)
+        const people = await fetchData(`${API}${PEOPLE_URL.replace(':id', id)}`)
+        console.log(people.name)
+        console.log(people.gender)
     } catch(error) {
         console.error(error)
     }
 }
 
-console.log('before')
-theFunction(API, 1)
-console.log('after')
+theFunction(5)
